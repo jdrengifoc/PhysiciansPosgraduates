721d0154b34e830fd9df9c208d7e0f4a79029b0b
Juan David Rengifo Castro <jdrengifoc@eafit.edu.co>
2024-06-26 21:18:43 -0500

    Use dataRC for 1_get_mini_PILA.R and minor changes.

--- Code/1_get_mini_PILA.R
+++ Code/1_get_mini_PILA.R
@@ -1,9 +1,10 @@
 # Get PILA history --------------------------------------------------------
+library(dataRC)
 tic()
-folder <- 'Z:/Christian Posso/_banrep_research/datos_originales/_PILA/'
+folder <- '//wmedesrv/gamma/Christian Posso/_banrep_research/datos_originales/_PILA/'
 files <- list.files(folder, pattern = '200[89]|20[12][0-9]')
 
-dict_path <- '../Data/PILA_dictionary.xlsx'
+dict_path <- '//wmedesrv/gamma/Christian Posso/_banrep_research/datos_originales/unidicts/PILA_dictionary.xlsx'
 dict <- read_excel(dict_path, sheet = 'colnames')
 names(dict) <- sapply(str_split(names(dict), '\\.'),
                       function(x) paste(tail(x, 2), collapse = '.'))
@@ -18,24 +19,12 @@ tic()
 for (file in files) {
   cat(paste('Began', file))
   tic()
-  # Auxiliary variables to select proper names and get the desired types. 
-  (df_selected <- filter(dict, uniname %in% selected_columns) %>% 
-      select(uniname, uniclass, file) %>% drop_na(file) %>% 
-      replace(is.na(.), ''))
-  (selected_columns_file <- df_selected[[file]])
-  (selected_columns1 <- df_selected$uniname)
-  (desired_classes <- df_selected$uniclass)
   
   df0 <- open_dataset(sprintf('%s/%s', folder, file)) %>%
-    # Select variables and rename.
-    select(all_of(selected_columns_file)) %>%
-    rename_at(vars(selected_columns_file), function(x) selected_columns1) %>%
+    dataRC::unify_colnames(dict, file, selected_columns) %>% 
     filter(personabasicaid %in% ids) %>%
-    # Unify column types.
-    mutate(
-      across(all_of(selected_columns1[desired_classes == 'numeric']), as.numeric),
-      across(all_of(selected_columns1[desired_classes == 'character']), as.character)
-    ) %>% collect
+    dataRC::unify_classes(dict, file, selected_columns) %>% 
+    collect
   
   df <- bind_rows(df, df0)
   
@@ -46,5 +35,3 @@ df %>%
   mutate(fecha_cobertura = ymd(str_sub(fecha_cobertura, 1L, 10L))) %>% 
   write_parquet('../New Methodology/PhysiciansPostgraduates/mini_history_PILA.parquet')
 sprintf('\n\t Completed in %f mins\n', get_values_tic_msg('min')) %>% cat
-
-
